dab8c862dc539f0ba680a4f16aa1673e5ade73b3
Jiri Oliva <o.krysa@seznam.cz>
2018-10-26 20:04:58 +0200

    Create initial project structure

---
+++ persistence/src/main/java/com/blackteam/pipboy/persistence/Test.java
@@ -0,0 +1,13 @@
+package com.blackteam.pipboy.persistence;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Test {
+  private static final Logger LOG = LogManager.getLogger(Test.class);
+
+  public static void main(String[] args) {
+    LOG.info("test logging");
+  }
+
+}
