3367fe21b1213c7a46b1b0dcc9d5de9e2556231a
Jiri Oliva <o.krysa@seznam.cz>
2018-10-27 16:15:13 +0200

    Create 'Person' entity, create persistence access for Person

---
+++ persistence/src/main/java/com/blackteam/pipboy/persistence/entity/Person.java
@@ -0,0 +1,102 @@
+package com.blackteam.pipboy.persistence.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.util.Objects;
+
+/**
+ * Entity class representing person (user).
+ *
+ * @author Jiri Oliva
+ */
+@Entity
+public class Person {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotNull
+  @Column(nullable = false)
+  private String name;
+
+  @NotNull
+  @Column(nullable = false)
+  private String surname;
+
+  @NotNull
+  @Column(nullable = false, unique = true)
+  @Pattern(regexp=".+@.+\\....?")
+  private String email;
+
+  private Boolean administrator;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public void setSurname(String surname) {
+    this.surname = surname;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Boolean getAdministrator() {
+    return administrator;
+  }
+
+  public void setAdministrator(Boolean administrator) {
+    this.administrator = administrator;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null) return false;
+    if (this.id == null) return true;
+    if (o instanceof Person) {
+      Person person = (Person) o;
+      return Objects.equals(id, person.id) &&
+              Objects.equals(email, person.email);
+    }
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, email);
+  }
+
+  @Override
+  public String toString() {
+    return "Person{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", surname='" + surname + '\'' +
+            ", email='" + email + '\'' +
+            ", administrator=" + administrator +
+            '}';
+  }
+}
