82537d27eaa0e3680a5e2899b22640207bb26692
Jakub Havrila <jakub.havrila@gmail.com>
2018-10-27 22:01:04 +0200

    Added Area entity and DAO

---
+++ persistence/src/main/java/com/blackteam/pipboy/persistence/dao/AreaDaoImpl.java
@@ -0,0 +1,56 @@
+package com.blackteam.pipboy.persistence.dao;
+
+import com.blackteam.pipboy.persistence.entity.Area;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+/**
+ * Implement AreaDao
+ *
+ * @author Jakub Havrila
+ */
+@Repository
+public class AreaDaoImpl implements AreaDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public Area findById(Long id){
+        return em.find(Area.class, id);
+    }
+
+    @Override
+    public void create(Area a) {
+        em.persist(a);
+    }
+
+    @Override
+    public void update(Area a) {
+        em.merge(a);
+    }
+
+    @Override
+    public void delete(Area a) {
+        em.remove(a);
+    }
+
+    @Override
+    public List<Area> findAll() {
+        return em.createQuery("select a from Area a", Area.class).getResultList();
+    }
+
+    @Override
+    public Area findByName(String name) {
+        try {
+            return em.createQuery("select a from Area a where name = :name", Area.class)
+                    .setParameter(":name", name).getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+}
