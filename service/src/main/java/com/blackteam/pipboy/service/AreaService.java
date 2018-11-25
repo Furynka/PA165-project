74f42890198dc882db3876141e1de46c6c834294
Jakub Havrila <jakub.havrila@gmail.com>
2018-11-25 18:14:04 +0100

    Area Service and Facade

---
+++ service/src/main/java/com/blackteam/pipboy/service/AreaService.java
@@ -0,0 +1,17 @@
+package com.blackteam.pipboy.service;
+
+import com.blackteam.pipboy.persistence.entity.Area;
+
+import java.util.List;
+
+/**
+ * Area service layer interface
+ */
+public interface AreaService {
+    Area create(Area area) throws IllegalArgumentException;
+    void update(Area area) throws IllegalArgumentException;
+    void remove(Area area) throws IllegalArgumentException;
+    Area findById(Long id) throws IllegalArgumentException;
+    Area findByName(String name) throws IllegalArgumentException;
+    List<Area> findAll();
+}
