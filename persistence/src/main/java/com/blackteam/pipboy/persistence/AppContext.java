21c9a25914de9f9e7d05121cf9d2fc78e0606ae7
Jirka Oliva <o.krysa@seznam.cz>
2018-11-26 18:59:02 +0100

    Add exception translation to DataAccessException

--- persistence/src/main/java/com/blackteam/pipboy/persistence/AppContext.java
+++ persistence/src/main/java/com/blackteam/pipboy/persistence/AppContext.java
@@ -5,6 +5,7 @@ import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
 import org.springframework.instrument.classloading.LoadTimeWeaver;
@@ -23,6 +24,15 @@ import javax.sql.DataSource;
 @EnableJpaRepositories
 @ComponentScan(basePackages = "com.blackteam.pipboy")
 public class AppContext {
+
+  /**
+   * Enables automatic translation of exceptions to DataAccessExceptions.
+   */
+  @Bean
+  public PersistenceExceptionTranslationPostProcessor postProcessor() {
+    return new PersistenceExceptionTranslationPostProcessor();
+  }
+
   @Bean
   public JpaTransactionManager transactionManager(){
     return  new JpaTransactionManager(entityManagerFactory().getObject());
