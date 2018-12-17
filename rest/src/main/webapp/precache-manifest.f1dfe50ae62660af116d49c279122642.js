004828bb4277147266ed8d34d44f765c378c87b3
Jiri Oliva <o.krysa@seznam.cz>
2018-12-17 23:52:02 +0100

    Add web app to rest module

---
+++ rest/src/main/webapp/precache-manifest.f1dfe50ae62660af116d49c279122642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2aadf9bdd10d8a43677f",
+    "url": "/pa165/static/js/runtime~main.2aadf9bd.js"
+  },
+  {
+    "revision": "4def6609db9edef6969c",
+    "url": "/pa165/static/js/main.4def6609.chunk.js"
+  },
+  {
+    "revision": "e6063a5acc0ef6ef80ee",
+    "url": "/pa165/static/js/1.e6063a5a.chunk.js"
+  },
+  {
+    "revision": "4def6609db9edef6969c",
+    "url": "/pa165/static/css/main.cf785291.chunk.css"
+  },
+  {
+    "revision": "e6063a5acc0ef6ef80ee",
+    "url": "/pa165/static/css/1.d0e87f2f.chunk.css"
+  },
+  {
+    "revision": "e55e4ab2fe0c816fb32f6f6d2474cc3f",
+    "url": "/pa165/index.html"
+  }
+];
